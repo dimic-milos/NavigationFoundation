dd473f5b7bcf67f3bb13e26c322d9c6764db50ba
Milos Dimic <milos.dimic@htecgroup.com>
2018-12-20 07:46:40 +0100

    Initial Commit

---
+++ NavigationFoundation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationFoundation
+//
+//  Created by Milos on 12/20/18.
+//  Copyright © 2018 Milos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
